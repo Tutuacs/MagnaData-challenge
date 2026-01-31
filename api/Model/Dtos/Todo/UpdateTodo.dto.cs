c720c8abe1257e300b3c981e9e9b7ad647bf2d03
Tutuacs <arthursilva.rs1@gmail.com>
2026-01-31 02:33:23 -0300

    fix: remove required from Description

--- api/Model/Dtos/Todo/UpdateTodo.dto.cs
+++ api/Model/Dtos/Todo/UpdateTodo.dto.cs
@@ -1,12 +1,11 @@
 namespace api.Model.Dtos.Todo;
 
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
 
 public class UpdateTodoDto
 {
-    [Required]
+
     [StringLength(int.MaxValue, MinimumLength = 10)]
-    public required string Description { get; set; }
+    public string? Description { get; set; }
     public Boolean? Completed { get; set; }
 }
